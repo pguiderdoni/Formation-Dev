e8cfcf44c07ebcafa9aee897eba1fd2161c5fdc8
pguiderdoni <101812669+pguiderdoni@users.noreply.github.com>
2022-06-13 16:52:17 +0200

    13/06 fin

--- JavaScript/script/script.js
+++ JavaScript/script/script.js
@@ -1,43 +1,12 @@
-// afficher le contenu du bouton Spoiler
-let spoilers = document.querySelectorAll(".spoiler");
-for (s of spoilers) {
-  let button = s.querySelector("button");
-  button.addEventListener("click", function (e) {
-    console.log("test");
-    let spoil = s.querySelector(".spoiler-content");
-    if (spoil.classList.contains("visible")) {
-      s.querySelector(".spoiler-content").classList.remove("visible");
-      this.innerHTML = "Afficher le spoil";
-    } else {
-      s.querySelector(".spoiler-content").classList.add("visible");
-      this.innerHTML = "Masquer le spoil";
-    }
-  });
+function drawRond() {
+  var canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Cercle extérieur
+    ctx.moveTo(110, 75);
+  }
 }
 
-// fonction pour masquer l'active et afficher le nouvel element
-let afficher = function (item) {
-  let li = item.parentNode;
-  let div = item.parentNode.parentNode.parentNode;
-  // trouve la div quo est active et enleve l'attribut 'active'
-  div.querySelector(".tabs .active").classList.remove("active");
-  // ajoute 'active' sur la <li> selectionnée
-  li.classList.add("active");
-  // enleve le texte correspondant
-  div.querySelector(".tabs-content .active").classList.remove("active");
-  // affiche le text qui a le meme ID que la <li>
-  div.querySelector(item.getAttribute("href")).classList.add("active");
-};
-//fonction qui attend un click sur un element <a>
-// crée une variable liste(qui contient tous les liens <a>)
-let tabs = document.querySelectorAll(".tabs a");
-//et boucle sur chaque element de la liste
-for (let tab of tabs) {
-  tab.addEventListener("click", function () {
-    // au click, on execute la fonction afficher()
-    afficher(tab);
-  });
-}
-// code pour rafraichir la page en restant sur le meme element
-let a = document.querySelector('a[href="' + window.location.hash + '"]');
-afficher(a);
+let canvas = document.querySelectorAll("");
